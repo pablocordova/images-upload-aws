397c082638d5b49d375f1d7e78d98eeb2fcaaa56
pablocordova <pccm_77@hotmail.com>
2021-11-14 20:41:16 -0500

    Roolback production

--- client/src/env.tsx
+++ client/src/env.tsx
@@ -1,6 +1,6 @@
 export const env = {
   AUTH_ADDRESS: "https://raysecur-001.auth.us-east-2.amazoncognito.com",
   CLIENT_ID: "66mn9kmr2p06hrl4bombr1r294",
-  REDIRECT_URI: "http://18.188.12.132",
+  REDIRECT_URI: "http://localhost:3000",
   API_ADDRESS: "https://mgtxx7581l.execute-api.us-east-2.amazonaws.com/prod"
 }
